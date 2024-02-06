ea355db150d91445d87edb69378b50737386cd94
Hakan Taşdelen <hakann.tasdelenn@gmail.com>
2024-02-06 20:12:05 +0300

    Customer class'ına ait field lar, consrtuctor, get set metotları, toString metodu OK

--- src/Customer.java
+++ src/Customer.java
@@ -9,17 +9,29 @@ public class Customer {
      customerId Integer,
      firstName, lastName, city String OLACAK
      */
-
+    private int customerId;
+    private String firstName;
+    private String lastName;
+    private String city;
+    private static int nextId=1000;
 
 
     ///////////////////////////////////////////////////////////////////////////////////
 
     // CONSTRUCTORS
     // TODO BOŞ CONSTRUCTOR OLUŞTURUN
+
+    public Customer() {
+    }
+
     // PARAMTRELERİ int customerId, String firstName, String lastName, String city OLAN CONSTRUCTOR OLUŞTURUN
 
-
-
+    public Customer( String firstName, String lastName, String city) {
+        this.customerId = ++nextId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+    }
 
 
     ////////////////////////////////////////////////////////////////////////////////////
@@ -27,17 +39,39 @@ public class Customer {
     // GETTER SETTER METHODS
     // TODO  GETTER VE SETTER METODLARI OLUŞTURUN
 
+    public int getCustomerId() {
+        return customerId;
+    }
 
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public String getLastName() {
+        return lastName;
+    }
 
+    public String getCity() {
+        return city;
+    }
+
+//    public void setCustomerId(int customerId) {
+//        this.customerId = customerId;
+//    }
 
     ////////////////////////////////////////////////////////////////////////////////
 
     //  TODO TO STRING METODU OLUŞTURUN
 
-
-
-
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerId=" + customerId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
 
 
     /////////////////////////////////////////////////////////////////////////////////
