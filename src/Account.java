bc5417a17a4a577fb4c2b8787d1b1138f6e068eb
Hakan Taşdelen <hakann.tasdelenn@gmail.com>
2024-02-06 20:29:04 +0300

    Account class'ı nın para çekme ve para yatırma metodları tmamlandı

--- src/Account.java
+++ src/Account.java
@@ -53,12 +53,29 @@ public class Account {
       3. BÜYÜKSE BAKİYEYE İLAVE EDİP KULLANICIYA MESAJ VERİN
       4. YATIRILMAK İSTENEN TUTAR SIFIRDAN KÜÇÜKSE KULLANICIYA MESAJ VERİN, YENİDEN DENEMESİNİ İSTEYİN...
      */
+        if (amount >=0 ) {
+            balance += amount;
+            System.out.println( amount + " TL, hesabınıza aktarıldı" );
+            System.out.println("Bakiye (TL) = " + balance);
+        }else {
+            System.out.println("Yatırılacak tutar sıfırdan büyük olmalıdır; Tekrar deneyiniz.");
+        }
+/**
+ * tutar>0 ise
+ *      bakiye = bakiye + tutar
+ *      tutar "hesabınıza aktarıldı"
+ *      "güncel bakiye =" bakiye
+ *  değilse
+ *      "yatırılacak tutar sıfırdan büyük olmalı, lütfen tekrar deneyiniz..
+ *
+ *
+ */
+
+}
 
 
 
 
-
-    }
     ////////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -74,6 +91,20 @@ public class Account {
          4. ÇEKİLMEK İSTENEN TUTAR (amount) POZİTİFSE VE BAKİYEDEN KÜÇÜKSE,BAKİYEDEN DÜŞÜP,YENİ BAKİYEYİ KULLANICIYA BİLDİRİN...
          5. YATIRILMAK İSTENEN TUTAR SIFIRDAN KÜÇÜKSE YA DA BAKİYEDEN BÜYÜKSE,KULLANICIYA MESAJ VERİN, YENİDEN DENEMESİNİ İSTEYİN
          */
+        if(amount >= 0)
+        {
+            if (amount <= balance)
+            {
+                balance -= amount;
+                System.out.println(amount + " TL hesabınızdan çekildi.");
+                System.out.println("Güncel bakiye = " +balance);
+            }else {
+                System.out.println(amount + " TL tutarı için, yetersiz bakiye");
+                System.out.println("Güncel bakiye = " +balance);
+            }
+
+        }else System.out.println("Çekilecek tutar sıfırdan büyük olmalıdır; Tekrar deneyiniz.");
+
 
 
 
